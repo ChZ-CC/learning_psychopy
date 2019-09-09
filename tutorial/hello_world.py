3fc15e1b4a13da9c67befe69a0147bc28f7eaea5
congcong.wang <congcong.wang@17zuoye.com>
2019-09-09 15:54:52 +0800

    init

---
+++ tutorial/hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from psychopy import visual, core
+
+win = visual.Window()
+msg = visual.TextStim(win, text=u"\u00A1Hola mundo!")
+
+msg.draw()
+win.flip()
+core.wait(1)
+win.close()
